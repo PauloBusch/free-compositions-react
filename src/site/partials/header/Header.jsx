145c017beb7d4bfd3a6fad6a58365dfe01439c23
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-02-27 20:33:41 -0300

    Responsive

--- src/site/partials/header/Header.jsx
+++ src/site/partials/header/Header.jsx
@@ -3,7 +3,23 @@ import './Header.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const INITIAL_STATE = { show: false };
+
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = INITIAL_STATE;
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({ 
+      ...this.state, 
+      show: !this.state.show 
+    });
+  }
+
   render() {
     return (
       <header className="site-header">
@@ -12,10 +28,15 @@ class Header extends Component {
             <img src="images/free-compositions/logo-small.png"/>
             <h1>{ this.props.name }</h1>
           </a>
+          <i id="toggle-mobile" onClick={ this.toggleMenu } className="fas fa-bars"></i>
         </div>
-        <div className="menu">
+        <div className="menu-desktop">
           <i className="fas fa-bars"></i>
           <span>|</span>
+          <a href="#">Inscrever-se</a>
+          <a href="#">Entrar</a>
+        </div>
+        <div className={ `menu-mobile ${this.state.show ? 'show' : ''}` }>
           <a href="#">Inscrever-se</a>
           <a href="#">Entrar</a>
         </div>
