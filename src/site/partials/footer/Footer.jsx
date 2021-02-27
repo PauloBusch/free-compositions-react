760e10e354b4fcb4e6708ea2eca8ee582280e6de
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-02-27 19:07:20 -0300

    Footer

--- src/site/partials/footer/Footer.jsx
+++ src/site/partials/footer/Footer.jsx
@@ -4,6 +4,42 @@ import React from 'react';
 
 export default props => (
   <footer>
-    footer
+    <div className="details">
+      <img src="images/free-compositions/logo-strong.png"/>
+      <div className="card">
+        <h2>Empresa</h2>
+        <ul>
+          <li><a href="#">Quem somos</a></li>
+          <li><a href="#">Ajuda</a></li>
+        </ul>
+      </div>
+      <div className="card">
+        <h2>Comunidade</h2>
+        <ul>
+          <li><a href="#">Todos os canais</a></li>
+          <li><a href="#">Mais ouvidas</a></li>
+          <li><a href="#">Novos lançamentos</a></li>
+        </ul>
+      </div>
+      <div className="card">
+        <h2>Links úteis</h2>
+        <ul>
+          <li><a href="#">Aviso legal</a></li>
+          <li><a href="#">Termos e condições de uso</a></li>
+          <li><a href="#">Política de privacidade</a></li>
+        </ul>
+      </div>
+    </div>
+    <div className="social">
+      <div className="icons">
+        <a href="#"><i className="fab fa-instagram"></i></a>
+        <a href="#"><i className="fab fa-twitter"></i></a>
+        <a href="#"><i className="fab fa-facebook"></i></a>
+        <a href="#"><i className="fab fa-youtube"></i></a>
+      </div>
+      <a href="https://gritealto.com/" target="_blank">
+        Powered by GriteAlto.
+      </a>
+    </div>
   </footer>
 );
